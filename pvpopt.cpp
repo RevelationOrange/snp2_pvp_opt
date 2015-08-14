ca3e3f1c4defa7f748fdd328cb9826af625be3b3
RevelationOrange <RevelationOrange@gmail.com>
2015-08-13 23:17:57 -0700

    let's get this party going
    just getting everything started, incomplete skeleton of program provided

---
+++ pvpopt.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// this will just be the skeleton of the program for now, feel free to fill in whatever
+
+class itemTableEntry
+{
+	string name, category, worker, maxRes, station, iclass;
+	float craftTime;
+	bool rare;
+	int price, cxp, sxp, level, iron, wood, herbs, leather, fabric, powder,
+	dye, elfwood, glass, oil, ironwood, steel, crystal, mithril, gems, acid,
+	tears, rocks, shells, feathers, ice, horns, moons, powders, silk, shadows,
+	scales, suns, leaves, totalRares, atkVal, id;
+	
+}
+
+class customer
+{
+	string name, cclass, attack;
+	int level;
+	// some type to be determined, probly a vector: someType catList, configList;
+}
+
+int itemSearch(item)
+{
+	// for (x in masterItemList)
+	// if (x.name.lower() == item || x.name == item) { return x.id; }
+}
+
+int main()
+{
+	int topXlist = 10;
+	// int krownLookup[] = [[5,100],[10,300],[15,600],[20,1000],[25,1500],[30,2100],[35,2800],[40,3600],[45,4500],[50,5500],[55,6500],[60,7750],[65,9000],[70,10500],[75,12000],[80,14000],[85,16000],[90,18500],[95,21000],[100,24000]]
+	// nKrownGoals = krownLookup.length or length(krownLookup);
+	
+}
--- readme.txt
+++ readme.txt
@@ -1,5 +1,5 @@
-dkwjndi u1n9 1jn 31
- f13 f3
-1if 3p fmipfn 3
-1 
-alrighty
+this is a project to make a program that can do what the python version I wrote does, only in a reasonable amount of time
+
+here's the python code: https://gist.github.com/RevelationOrange/220a84d7b4a67ef20ef3
+
+I want to set it up so anyone can contribute changes, it seems like github is a good way to do that
